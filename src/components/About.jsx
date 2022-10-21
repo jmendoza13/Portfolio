a788c83e3f35740d4bd78c1c02633970fd954323
jmendoza13 <justinmendoza13@gmail.com>
2022-10-21 15:12:23 -0400

    portfolio updated. project links not working yet

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+        My name is Justin Mendoza. I'm a first generation, Latino-American 2021 graduate from Bloomsburg University of PA with a B.A. in Communication Studies. I offer several years of professional experience in the healthcare industry, holding positions in bookkeeping, operations/administration and sales. I’m seeking to further my career in healthcare, with an opportunity to work in a software engineering role that assists merchants, vendors, and consumers. I offer strong front-end web development skills with a focus in MERN stack applications, and CSS libraries such as Bootstrap 5 and Tailwind.
+        </p>
+        <p className="text-xl mt-20">
+        I'm a Goal-oriented individual, who offers several years of experience in leadership positions. Passionate about
+        proving efficiency in productivity. Offering a diverse background, with experience across multiple industries,
+        specifically in healthcare administration & business. Mentally flexible in approaching new challenges with an
+        outside the box approach. Seeking collaboration and motivation through coaching based management while
+        creating effective solutions in a timely manner.
+        </p>
+        <br />
+
+        <p className="text-xl">
+          
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import cryptoChat from "../assets/portfolio/cryptoChat.jpg";
+import connect4 from "../assets/portfolio/connect4.jpg";
+import safeTravels from "../assets/portfolio/safeTravels.jpg";
+import limitlessStylus from "../assets/portfolio/limitlessStylus.jpg";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: cryptoChat,
+    },
+    {
+      id: 2,
+      src: connect4,
+    },
+    {
+      id: 3,
+      src: safeTravels,
+    },
+    {
+      id: 4,
+      src: limitlessStylus,
+    },
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
